e6bc0aa0000beaba4482e139513d841afc2a5df8
Mathias Wöß <mat.woess@gmail.com>
2025-02-06 01:10:49 +0100

    - add custom Javadoc task that aggregates each of the three modules - include links to other api docs - exclude module-info-java files to avoid "too many module declarations" errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,23 @@
 plugins {
+    java
     id("com.vanniktech.maven.publish") version "0.30.0" apply false
+}
+
+tasks.register<Javadoc>("aggregateJavadocs") {
+    setDestinationDir(file("${layout.buildDirectory.get()}/docs/aggregate"))
+    source = files(subprojects.flatMap {
+        it.sourceSets.main.get().allJava.filter { f -> f.name != "module-info.java" }
+    }).asFileTree
+    classpath = files(subprojects.flatMap { it.sourceSets.main.get().compileClasspath })
+    title = "All Modules"
+
+    options {
+        this as StandardJavadocDocletOptions
+        encoding = "UTF-8"
+        memberLevel = JavadocMemberLevel.PROTECTED
+        links = listOf(
+            "https://docs.oracle.com/javase/21/docs/api/",
+            "https://download.java.net/java/GA/javafx20.0.1/docs/api/"
+        )
+    }
 }

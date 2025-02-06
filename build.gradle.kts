883e49b10266e8fe69f2aa71d715912ffb1980f4
Mathias Wöß <mat.woess@gmail.com>
2025-02-07 00:24:30 +0100

    update task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.30.0" apply false
 }
 
-tasks.register<Javadoc>("aggregateJavadocs") {
-    setDestinationDir(file("${layout.buildDirectory.get()}/docs/aggregate"))
+tasks.register<Javadoc>("aggregateJavadoc") {
+    title = "All Packages"
     source = files(subprojects.flatMap {
+        // filter module-info.java files to avoid conflicts of multiple modules on same level
         it.sourceSets.main.get().allJava.filter { f -> f.name != "module-info.java" }
     }).asFileTree
     classpath = files(subprojects.flatMap { it.sourceSets.main.get().compileClasspath })
-    title = "All Modules"
-
+    setDestinationDir(file("${layout.buildDirectory.get()}/docs/javadoc"))
     options {
         this as StandardJavadocDocletOptions
         encoding = "UTF-8"
